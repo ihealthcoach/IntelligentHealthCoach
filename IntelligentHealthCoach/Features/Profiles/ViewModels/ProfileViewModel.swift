84169ac90f6f1f8031375ba7a7d3f1129a1075d5
Casper Broe <casper@ihealthcoach.io>
2025-02-27 13:21:00 +0100

    #1

--- IntelligentHealthCoach/Features/Profiles/ViewModels/ProfileViewModel.swift
+++ IntelligentHealthCoach/Features/Profiles/ViewModels/ProfileViewModel.swift
@@ -5,3 +5,36 @@
 //  Created by Casper Broe on 26/02/2025.
 //
 
+// ProfileViewModel.swift
+import SwiftUI
+import Combine
+
+class ProfileViewModel: ObservableObject {
+    @Published var workoutsCount: Int = 0
+    @Published var streakDays: Int = 0
+    @Published var achievementsCount: Int = 0
+    @Published var toggleStates: [Int: Bool] = [:]
+    
+    // Account, workout, more options referenced in ProfileView
+    var accountOptions: [SettingOption] = [
+        SettingOption(id: 1, title: "Profile", iconName: "person"),
+        SettingOption(id: 2, title: "Questionnaire", iconName: "list.clipboard"),
+        // Add remaining options from ProfileView
+    ]
+    
+    var workoutOptions: [SettingOption] = [
+        // Copy from ProfileView
+    ]
+    
+    var moreOptions: [SettingOption] = [
+        // Copy from ProfileView
+    ]
+    
+    func loadUserProfile() async {
+        // Implementation to load profile data
+    }
+    
+    func handleSettingTap(_ option: SettingOption) {
+        // Implementation to handle setting tap
+    }
+}
