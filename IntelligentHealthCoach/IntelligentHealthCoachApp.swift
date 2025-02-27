3a32d7fbf2f67ee71b852dea6044098704d3acf7
Casper Broe <casper@ihealthcoach.io>
2025-02-27 10:30:33 +0100

    Initial Commit

---
+++ IntelligentHealthCoach/IntelligentHealthCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligentHealthCoachApp.swift
+//  IntelligentHealthCoach
+//
+//  Created by Casper Broe on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntelligentHealthCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
